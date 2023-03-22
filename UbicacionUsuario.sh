72853b190058e1515aff72896128526182c68bb0
Jonathan Garcia <jonathan.jaet@gmail.com>
2023-03-22 03:38:43 -0600

    [FEATURE]Agregue pruebas del 2 y 3 scriptxterminar

--- UbicacionUsuario.sh
+++ UbicacionUsuario.sh
@@ -4,3 +4,15 @@ echo $USER
 
 pwd
 
+# fin del segundo script
+# inicio del tercer script
+
+cat .bashrc
+
+echo "alias lm='ls -la'" >> .bashrc
+
+source ~/.bashrc
+
+cat .bashrc
+
+# no funciona aún
