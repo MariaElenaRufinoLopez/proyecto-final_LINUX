f735c95c0908ed02aa1e9d60305b0741833996cc
Jonathan Garcia <jonathan.jaet@gmail.com>
2023-03-22 04:13:46 -0600

    [FEATURE] Se intentaron hacer cambios, no works

--- UbicacionUsuario.sh
+++ UbicacionUsuario.sh
@@ -9,10 +9,25 @@ pwd
 
 cat .bashrc
 
-echo "alias lm='ls -la'" >> .bashrc
+echo 'alias lm="ls -la"' >> .bashrc
 
 source ~/.bashrc
 
 cat .bashrc
 
 # no funciona aún
+
+echo 'alias yaquedo="cd ..; ls -la"' >> .bashrc
+
+cat .bashrc
+
+source .bashrc
+
+
+PATH=$(dirname "${BASH_SOURCE[0]}")/bin:$PATH
+export PATH
+porfin() {
+    cd ..; ls -la
+}
+
+# se intentó de otra forma y no funcionó, seguire intentando y borrare mañana lo que no funcionó
